e617eb9ae378dc214887ab9398b9579b4ed58596
tuyojr <olutuyod@gmail.com>
2022-09-12 09:59:25 +0100

    if statement

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 
 function helloFunc(){
+    if [[ -z "$1" ]]; then
+    echo "Empty Argument passed"
+    else
     echo "Hello $1, welcome to today's session."
-} 
+    fi
+}
 
-helloFunc "tuyojr"
+helloFunc
