c523739e824ae3320051848aa8dec86e1e739cbd
tuyojr <olutuyod@gmail.com>
2022-10-06 10:43:00 +0100

    updated to read tuyojr global variable

--- conditionals.sh
+++ conditionals.sh
@@ -2,12 +2,6 @@
 echo "Please enter your name: "
 read Name
 
-# I'm getting a user input to set username because I'm not
-# running this script in a linux environment. 
-# I'm using gitbash.
-echo "Please enter your username: "
-read USER
-
 # using the if statement to compare two variables
 if [ $Name != $USER ]
 then
@@ -21,7 +15,9 @@ fi
 
 if [ "$Name" != $USER ]
 then
-    echo "(Empty Name): Your name and username are not the same."
+    echo "(in case of empty Name): Your name and username are not the same."
 else
-    echo "(Empty Name): Your name and username are the same."
+    echo "(in case of empty Name): Your name and username are the same."
 fi
+
+
