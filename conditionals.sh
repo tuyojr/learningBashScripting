d3ff93ec2e0b43c8b20e600337d00404795034d5
tuyojr <olutuyod@gmail.com>
2022-10-06 11:50:33 +0100

    using the && and || operator with if statements

--- conditionals.sh
+++ conditionals.sh
@@ -20,4 +20,19 @@ else
     echo "(in case of empty Name): Your name and username are the same."
 fi
 
+# using the && and || operators
 
+echo "Please enter your age: "
+read Age
+
+if [ "$Name" == "tuyojr" ] && [ "$Age" -le 21 ]
+then
+    echo "This runs because the user's name is $Name and the user's age is $Age, which is less than or equal to 21, the required age."
+else
+    echo "This does not work because the user's name is $Name not tuyojr and the user's age, $Age is greater than 21."
+fi
+
+if [ "$Name" == "tuyojr" ] || [ "$Name" == "vagrant" ]
+then
+    echo "This will run if the user's $Name is tuyojr or vagrant."
+fi
